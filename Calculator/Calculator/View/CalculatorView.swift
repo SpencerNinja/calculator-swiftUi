2023273ad8e4005b903db16ba3ba34ae064713a1
SpencerNinja <spencer.ninja@gmail.com>
2022-02-26 12:04:55 -0700

    refactored button layout

--- Calculator/Calculator/View/CalculatorView.swift
+++ Calculator/Calculator/View/CalculatorView.swift
@@ -46,33 +46,36 @@ struct CalculatorView: View {
                     )
                     .cornerRadius(10)
                 Spacer()
-                VStack {
-                    HStack {
-                        NumberKeyButton(keyNumber: "1")
-                        NumberKeyButton(keyNumber: "2")
-                        NumberKeyButton(keyNumber: "3")
+                HStack {
+                    VStack {
+                        HStack {
+                            NumberKeyButton(keyNumber: "1")
+                            NumberKeyButton(keyNumber: "2")
+                            NumberKeyButton(keyNumber: "3")
+                        }
+                        HStack {
+                            NumberKeyButton(keyNumber: "4")
+                            NumberKeyButton(keyNumber: "5")
+                            NumberKeyButton(keyNumber: "6")
+                        }
+                        HStack {
+                            NumberKeyButton(keyNumber: "7")
+                            NumberKeyButton(keyNumber: "8")
+                            NumberKeyButton(keyNumber: "9")
+                        }
+                        HStack {
+                            NumberKeyButton(keyNumber: ".")
+                            NumberKeyButton(keyNumber: "0")
+                            EqualKeyButton(equalKey: "=")
+                        }
+                    }
+                    VStack {
+                        OperatorKeyButton(keyOperator: "➕")
+                        OperatorKeyButton(keyOperator: "➖")
+                        OperatorKeyButton(keyOperator: "✖️")
                         OperatorKeyButton(keyOperator: "➗")
                     }
-                    HStack {
-                        NumberKeyButton(keyNumber: "4")
-                        NumberKeyButton(keyNumber: "5")
-                        NumberKeyButton(keyNumber: "6")
-                        OperatorKeyButton(keyOperator: "✖️")
-                    }
-                    HStack {
-                        NumberKeyButton(keyNumber: "7")
-                        NumberKeyButton(keyNumber: "8")
-                        NumberKeyButton(keyNumber: "9")
-                        OperatorKeyButton(keyOperator: "➖")
-                    }
-                    HStack {
-                        NumberKeyButton(keyNumber: ".")
-                        NumberKeyButton(keyNumber: "0")
-                        EqualKeyButton(equalKey: "=")
-                        OperatorKeyButton(keyOperator: "➕")
-                    }
-                }
-                .font(.system(size: 50, weight: .bold))
+                }.font(.system(size: 50, weight: .bold))
             }
         }
         .environmentObject(calculatorViewModel)
